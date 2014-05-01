7d6523860622bfd2b9ea904159f2bdcf9f4d2dce
Joshcline002 <joshcline002@gmail.com>
2014-05-01 16:12:08 -0500

    "Visitor, badge reader, and employee has more information added"

--- controllers/security_analysis.py
+++ controllers/security_analysis.py
@@ -2,6 +2,7 @@
 from db import get_database_support
 from controllers import BaseController
 from personnel import PersonnelController
+from building_security import BuildingSecurityController
 from models.log_entry import BadgeReaderLogEntry
 from models.log_entry import EmployeeLogEntry
 from models.log_entry import VisitorLogEntry
@@ -21,15 +22,18 @@ class SecurityAnalysisController(BaseController):
         return self.db.dump_employee_log_entries()
 
     def visitor_entry(self, entry_id, badge_id, time, location, result):
-        log_entry = VisitorLogEntry(entry_id=entry_id, badge_id=badge_id, time=time, location=location, result=result)
+        visitor = PersonnelController().get_visitor(badge_id)
+        log_entry = VisitorLogEntry(entry_id=entry_id, badge_id=badge_id, name=visitor.name, associated_employee = visitor.associated_employee, time=time, location=location, result=result)
         return self.db.put_visitor_log_entry(log_entry)
 
     def get_visitor_log_entry_debug(self):
         return self.db.dump_visitor_log_entries()
 
     def badge_reader_entry(self, entry_id, badge_reader_id, badge_id, time, result):
-        log_entry = BadgeReaderLogEntry(entry_id=entry_id, badge_reader_id=badge_reader_id,
-                                        badge_id=badge_id, time=time, result=result)
+        reader=BuildingSecurityController().get_badge_reader(badge_reader_id)
+        door=BuildingSecurityController().get_door(reader.door_id)
+        log_entry = BadgeReaderLogEntry(entry_id=entry_id, badge_reader_id=badge_reader_id, door_id=reader.door_id, 
+                                        location=door.location, badge_id=badge_id, time=time, result=result)
         return self.db.put_badge_reader_log_entry(log_entry)
 
     def get_badge_reader_log_entry_debug(self):
@@ -53,11 +57,11 @@ class SecurityAnalysisController(BaseController):
         entries = self.db.get_all_visitor_entries()
 
         report = '\n\n'
-        report += '   ID    \t|   Badge\t|   Time\t\t| Location\t|   Result\n'
+        report += '   ID    \t|   Badge\t|     Name\t|     Associated Employee\t|   Time\t\t| Location\t|   Result\n'
         report += '--------------------------------------------------------------------\n'
         for entry in entries:
-            report += '   %s  \t|   %s\t|   %s\t|     %s\t|   %s\n' % (entry.entry_id, entry.badge_id,
-                                                               entry.time, entry.location, entry.result)
+            report += '   %s  \t|   %s\t|   %s\t|      %s\t|       %s\t|     %s\t|   %s\n' % (entry.entry_id, entry.badge_id, entry.name,
+                                                               entry.associated_employee, entry.time, entry.location, entry.result)
 
         return report
 
@@ -65,11 +69,13 @@ class SecurityAnalysisController(BaseController):
         entries = self.db.get_all_badge_reader_entries()
 
         report = '\n\n'
-        report += '   ID    \t|   Reader\t|   Badge\t|   Time\t\t|   Result\n'
+        report += '   ID     |  Reader  |  Door  |  Location  |  Badge  |  Time        |  Result\n'
         report += '------------------------------------------------------------------------------------\n'
         for entry in entries:
-            report += '   %s  \t|   %s\t|   %s\t\t|   %s\t|   %s\n' % (entry.entry_id,
+            report += '   %s  |  %s  |  %s  |  %s  |  %s  |  %s  |  %s\n' % (entry.entry_id,
                                                                          entry.badge_reader_id,
+                                                                         entry.door_id,
+                                                                         entry.location,
                                                                          entry.badge_id,
                                                                          entry.time,
                                                                          entry.result)
